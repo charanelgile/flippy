b06e601b0fa04f19b87b17aa0d2ab46d6a411e5e
Brian Raguine <brianraguine0628@gmail.com>
2023-08-10 23:19:55 +0800

    Making Sign Up Form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import CardGrid from "./components/CardGrid";
 import "./styles/style.css";
+import Header from './components/header';
+import  SignUpForm from './components/PlayerSignUp'
 
 function App() {
   return (
     <div className="App">
       <h1>Flippy</h1>
       <CardGrid />
+      <Header/>
+      <SignUpForm/>
     </div>
   );
 }
