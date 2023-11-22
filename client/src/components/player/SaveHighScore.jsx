4eb818bbe0d9a1a8c29c01c49d70b97c3526bc4a
Charanel Gile <charanelgile@icloud.com>
2023-11-22 13:34:23 +0800

    Add Save High Score and Reset Game Functionalities

--- client/src/components/player/SaveHighScore.jsx
+++ client/src/components/player/SaveHighScore.jsx
@@ -1,182 +1,165 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { CurrentSessionContext } from '../../contexts/CurrentSessionContext';
 
-const SaveHighScore = ({ currentSession }) => {
-  const {
-    playerID,
-    playerName,
-    playerCodename,
-    playerEmail,
-    playerTotalScore,
-    playerHighScore,
-  } = currentSession;
+const getToken = () => {
+  return localStorage.getItem('token');
+};
+
+const SaveHighScore = () => {
+  let goToLevel1 = useNavigate();
+  let goToRanking = useNavigate();
+
+  const token = getToken();
+
+  const { currentSession, setCurrentSession } = useContext(
+    CurrentSessionContext
+  );
 
   const restartGame = () => {
-    localStorage.removeItem("currentSession");
-    window.location.reload();
+    let currentPlayer = [
+      {
+        playerID: currentSession[0].playerID,
+        playerName: currentSession[0].playerName,
+        playerCodename: currentSession[0].playerCodename,
+        playerEmail: currentSession[0].playerEmail,
+        playerTotalScore: 0,
+        playerHighScore: currentSession[0].playerHighScore,
+        playerLevel: 1,
+        playerGridDimensions: [
+          'grid3x4',
+          'grid4x4',
+          'grid4x5',
+          'grid4x6',
+          'grid5x6',
+          'grid6x6',
+          'grid6x7',
+          'grid6x8',
+        ],
+      },
+    ];
+
+    setCurrentSession(currentPlayer);
+
+    goToLevel1('/Play');
+  };
+
+  const saveHighScore = async (event) => {
+    event.preventDefault();
+
+    const personalBest = {
+      highscore: currentSession[0].playerHighScore,
+    };
+
+    try {
+      const response = await fetch(
+        `/api/players/update/${currentSession[0].playerID}`,
+        {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(personalBest),
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+
+        console.log(data);
+      }
+
+      goToRanking('/Ranking');
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <div
-      id="divSaveHighScore"
-      className="d-flex flex-column justify-content-start align-items-center text-warning">
-      <h1 className="mb-4">Thank you for playing our game</h1>
+      id='divSaveHighScore'
+      className='d-flex flex-column justify-content-start align-items-center text-warning'>
+      <h1 className='mb-4'>Thank you for playing the game</h1>
 
-      <div id="cardSaveHighScore" className="card bg-warning py-3">
-        {playerID === 1111 ? (
-          <div>
-            <p>Total Score: {playerTotalScore}</p>
-            <p>High Score: {playerHighScore}</p>
-
-            <Link to={"/SignInPlayer"} className="btn btn-warning">
-              Save High Score
-            </Link>
-          </div>
-        ) : (
-          <form
-            id="frmSaveHighScore"
-            action="http://localhost:4000/player/savehighscore"
-            method="POST">
-            <fieldset className="container-fluid">
-              <div className="row w-100 m-0 p-0">
-                <div className="col-4 text-start">
-                  <label htmlFor="player_id" className="lblSaveHighScore">
-                    Player ID:
-                  </label>
-                </div>
-
-                <div className="col-8">
-                  <input
-                    type="text"
-                    id="player_id"
-                    name="player_id"
-                    className="fldSaveHighScore form-control"
-                    readOnly
-                    value={playerID}
-                  />
-                </div>
+      <div id='cardSaveHighScore' className='card bg-warning py-3'>
+        <form onSubmit={saveHighScore}>
+          <fieldset className='container-fluid'>
+            <div className='row w-100 m-0 p-0'>
+              <div className='col-4 text-start'>
+                <label htmlFor='codename' className='lblSaveHighScore'>
+                  Code Name:
+                </label>
               </div>
 
-              <div className="row w-100 m-0 p-0">
-                <div className="col-4 text-start">
-                  <label htmlFor="player_name" className="lblSaveHighScore">
-                    Full Name:
-                  </label>
-                </div>
+              <div className='col-8'>
+                <input
+                  type='text'
+                  id='codename'
+                  name='codename'
+                  className='fldSaveHighScore form-control'
+                  readOnly
+                  value={currentSession[0].playerCodename}
+                />
+              </div>
+            </div>
 
-                <div className="col-8">
-                  <input
-                    type="text"
-                    id="player_name"
-                    name="player_name"
-                    className="fldSaveHighScore form-control"
-                    readOnly
-                    value={playerName}
-                  />
-                </div>
+            <div className='row w-100 m-0 p-0'>
+              <div className='col-4 text-start'>
+                <label htmlFor='totalscore' className='lblSaveHighScore'>
+                  Total Score:
+                </label>
               </div>
 
-              <div className="row w-100 m-0 p-0">
-                <div className="col-4 text-start">
-                  <label
-                    htmlFor="player_code_name"
-                    className="lblSaveHighScore">
-                    Code Name:
-                  </label>
-                </div>
+              <div className='col-8'>
+                <input
+                  type='number'
+                  id='totalscore'
+                  name='totalscore'
+                  className='fldSaveHighScore form-control'
+                  readOnly
+                  value={currentSession[0].playerTotalScore}
+                />
+              </div>
+            </div>
 
-                <div className="col-8">
-                  <input
-                    type="text"
-                    id="player_code_name"
-                    name="player_code_name"
-                    className="fldSaveHighScore form-control"
-                    readOnly
-                    value={playerCodename}
-                  />
-                </div>
+            <div className='row w-100 m-0 p-0'>
+              <div className='col-4 text-start'>
+                <label htmlFor='highscore' className='lblSaveHighScore'>
+                  HighScore:
+                </label>
               </div>
 
-              <div className="row w-100 m-0 p-0">
-                <div className="col-4 text-start">
-                  <label htmlFor="player_email" className="lblSaveHighScore">
-                    Email:
-                  </label>
-                </div>
-
-                <div className="col-8">
-                  <input
-                    type="email"
-                    id="player_email"
-                    name="player_email"
-                    className="fldSaveHighScore form-control"
-                    readOnly
-                    value={playerEmail}
-                  />
-                </div>
+              <div className='col-8'>
+                <input
+                  type='number'
+                  id='highscore'
+                  name='highscore'
+                  className='fldSaveHighScore form-control'
+                  readOnly
+                  value={currentSession[0].playerHighScore}
+                />
               </div>
+            </div>
 
-              <div className="row w-100 m-0 p-0">
-                <div className="col-4 text-start">
-                  <label
-                    htmlFor="player_total_score"
-                    className="lblSaveHighScore">
-                    Total Score:
-                  </label>
-                </div>
+            <div className='d-flex justify-content-evenly'>
+              <button
+                type='submit'
+                id='btnSaveHighScore'
+                className='btn px-3 mx-3'>
+                Save High Score
+              </button>
 
-                <div className="col-8">
-                  <input
-                    type="number"
-                    id="player_total_score"
-                    name="player_total_score"
-                    className="fldSaveHighScore form-control"
-                    readOnly
-                    value={playerTotalScore}
-                  />
-                </div>
-              </div>
-
-              <div className="row w-100 m-0 p-0">
-                <div className="col-4 text-start">
-                  <label
-                    htmlFor="player_high_score"
-                    className="lblSaveHighScore">
-                    HighScore:
-                  </label>
-                </div>
-
-                <div className="col-8">
-                  <input
-                    type="number"
-                    id="player_high_score"
-                    name="player_high_score"
-                    className="fldSaveHighScore form-control"
-                    readOnly
-                    value={playerHighScore}
-                  />
-                </div>
-              </div>
-
-              <div className="d-flex justify-content-evenly">
-                <button
-                  type="submit"
-                  id="btnSaveHighScore"
-                  className="btn px-3 mx-3">
-                  Save High Score
-                </button>
-
-                <button
-                  type="button"
-                  id="btnRestartGame"
-                  className="btn btn-warning px-3 mx-3"
-                  onClick={restartGame}>
-                  Restart Game
-                </button>
-              </div>
-            </fieldset>
-          </form>
-        )}
+              <button
+                type='button'
+                id='btnRestartGame'
+                className='btn btn-warning px-3 mx-3'
+                onClick={restartGame}>
+                Restart Game
+              </button>
+            </div>
+          </fieldset>
+        </form>
       </div>
     </div>
   );
